926b0583083f68462dd297948a9a04c3667006e0
Yathendraindra <yathendra1997@gmail.com>
2025-08-22 12:50:55 +0530

    Changes pulled.

--- controller/install-alb-controller.sh
+++ controller/install-alb-controller.sh
@@ -12,6 +12,6 @@ helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
     --set clusterName=eks-portfolio-cluster \
     --set serviceAccount.create=false \
     --set serviceAccount.name=aws-load-balancer-controller \
-    --set vpcId=vpc-055bd29cf304ab09e \
+    --set vpcId=vpc-0e2a189f1cad6ffbb \
     --set region=ap-south-1
 
